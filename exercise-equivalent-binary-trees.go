18ef45687dafd20a0a883e71f19e24cf979aadab
Alexei Danchenkov <alexeid@scientologymedia.net>
2018-01-26 12:17:57 -0800

    updated with ANSI colors

--- exercise-equivalent-binary-trees.go
+++ exercise-equivalent-binary-trees.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/fatih/color"
 	"golang.org/x/tour/tree"
 )
-
-const Total int = 10
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
-	if t.Left != nil {
-		go Walk(t.Left, ch)
+	if t == nil {
+		return
 	}
-	if t.Right != nil {
-		go Walk(t.Right, ch)
-	}
+	Walk(t.Left, ch)
+	Walk(t.Right, ch)
 	ch <- t.Value
+}
+
+func WalkWrapper(t *tree.Tree, ch chan int) {
+	Walk(t, ch)
+	close(ch)
 }
 
 // Same determines whether the trees
@@ -25,15 +29,22 @@ func Same(t1, t2 *tree.Tree) bool {
 	match := make(map[int]int)
 	ch1 := make(chan int)
 	ch2 := make(chan int)
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
-	for i := 0; i < Total*2; i++ {
-		select {
-		case v := <-ch1:
-			match[v] += 1
-		case v := <-ch2:
-			match[v] += 1
+	go WalkWrapper(t1, ch1)
+	go WalkWrapper(t2, ch2)
+
+	for {
+		v1, ok1 := <-ch1
+		if !ok1 {
+			break
 		}
+		match[v1] += 1
+	}
+	for {
+		v2, ok2 := <-ch2
+		if !ok2 {
+			break
+		}
+		match[v2] += 1
 	}
 	for _, v := range match {
 		if v != 2 {
@@ -45,8 +56,26 @@ func Same(t1, t2 *tree.Tree) bool {
 
 func main() {
 	ch := make(chan int, 10)
-	go Walk(tree.New(1), ch)
+	go WalkWrapper(tree.New(1), ch)
 
-	fmt.Printf("tree.New(1) vs tree.New(1): Same? %t\n", Same(tree.New(1), tree.New(1)))
-	fmt.Printf("tree.New(1) vs tree.New(2): Same? %t\n", Same(tree.New(1), tree.New(2)))
+	for i := 0; i < 10; i++ {
+		v := <-ch
+		fmt.Printf("%d ", v)
+	}
+
+	fmt.Println()
+
+	fmt.Print("tree.New(1) == tree.New(1): ")
+	if Same(tree.New(1), tree.New(1)) {
+		color.Green("PASSED")
+	} else {
+		color.Red("FAILED")
+	}
+
+	fmt.Print("tree.New(1) != tree.New(2): ")
+	if !Same(tree.New(1), tree.New(2)) {
+		color.Green("PASSED")
+	} else {
+		color.Red("FAILED")
+	}
 }
